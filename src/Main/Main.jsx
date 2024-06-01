281b0545f8946b8750ceab606b58f890b445f0a8
EmranHossain <emran.shuvo.dev@gmail.com>
2024-06-01 13:39:17 +0600

    simple navbar added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,8 +1,11 @@
+
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div className="max-w-screen-xl mx-auto">
+            <Navbar />
             <Outlet />   
         </div>
     );
